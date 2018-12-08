760ba845b58bd71e05559596b3b880ee7d9dd219
James Xue <email@jxue.co>
2018-12-08 13:37:49 -0500

    centered result cards

--- my-app/src/components/advancedComponents/resultPanel.js
+++ my-app/src/components/advancedComponents/resultPanel.js
@@ -4,32 +4,30 @@ import { Card, CardTitle, CardText, Button, Row, Col } from "reactstrap";
 class ResultPanel extends Component {
   render() {
     return (
-      <div>
-        <div className='advanced-result-column'>
-          <div className='card'>
-            <Card body>
-              <CardTitle tag='h4'>Your Top Choices:</CardTitle>
-              <CardText>
-                <ol>
-                  <li>Coffee</li>
-                  <li>Tea</li>
-                  <li>Milk</li>
-                </ol>
-              </CardText>
-            </Card>
-          </div>
-          <div className='card'>
-            <Card body>
-              <CardTitle tag='h4'>Your Bottom Choices:</CardTitle>
-              <CardText>
-                <ol>
-                  <li>Coffee</li>
-                  <li>Tea</li>
-                  <li>Milk</li>
-                </ol>
-              </CardText>
-            </Card>
-          </div>
+      <div className='advanced-result-column'>
+        <div className='card'>
+          <Card body>
+            <CardTitle tag='h4'>Your Top Choices:</CardTitle>
+            <CardText>
+              <ol>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+              </ol>
+            </CardText>
+          </Card>
+        </div>
+        <div className='card'>
+          <Card body>
+            <CardTitle tag='h4'>Your Bottom Choices:</CardTitle>
+            <CardText>
+              <ol>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+              </ol>
+            </CardText>
+          </Card>
         </div>
       </div>
     );
