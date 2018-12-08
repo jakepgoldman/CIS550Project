671c570fe842b7c36bff62bfbcd1ded1408118d4
arnavjagasia <jagasiaa@wharton.upenn.edu>
2018-12-08 14:25:12 -0500

    Updating master home page

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+
 import Navigation from "./navigation";
-import Predefined from "./predefined";
-// import route from react router
+import SearchCard from "./homeComponents/searchCard";
+import About from "./homeComponents/about";
+
 import $ from 'jquery';
 const tasksURI = 'http://localhost:5000/family';
 
@@ -49,20 +51,19 @@ class Home extends Component {
 
   render() {
     return (
-        <Container>
-            <Row>
-                <h1 align="center"> Abode </h1>
-            </Row>
-            <Row>
-                <h4 align="center"> Arnav Jagasia, Jake Goldman, James Xue, Josh Doman </h4>
-                <p>
-                  At Penn, it is common for students to move to two places after
-                  graduation: New York City or San Francisco. We set out on a mission
-                  to prove there are many other places in the United States for you to live.
-                </p>
-            </Row>
-            <Predefined />
-          </Container>
+      <div className="scroll">
+        <div className="page">
+          <Navigation />
+          <div className="home">
+            <div className="search-card-container">
+              <SearchCard />
+            </div>
+          </div>
+        </div>
+        <div className="about">
+                <About />
+        </div>
+      </div>
     );
   }
 }
